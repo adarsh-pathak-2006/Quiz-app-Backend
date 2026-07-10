97c3e373e885fd1ea95d6816b297bfe019d76687
duckyduck01 <madhvetipathak2006@gmail.com>
2026-07-10 12:13:28 +0530

    added model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Quiz(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    question=models.TextField(null=True)
+    answer=models.TextField(null=True)
+
+    def __str__(self):
+        return self.user.first_name
+
